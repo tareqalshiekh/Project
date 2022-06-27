717aca2821963ef5c7ebaafc283395b568877325
yanal shoubaki <developer01@nextstagejo.com>
2022-06-27 12:33:59 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -62,3 +62,13 @@ Route::get('all_banner_ar', function () {
 
 
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
